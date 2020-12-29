f2da3d26fa61d9b0161c5319668c9b7089637df7
Sai Manudeep Gadde <38970629+Manudeep-git@users.noreply.github.com>
2020-12-29 12:42:24 +0530

    Add files via upload
    Missing-number optimal solution

---
+++ missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+	
+	//optimal solution using O(1) space complexity and O(n) time complexity
+    public int missingNumber(int[] nums) {
+		//nums =[0,1,3]
+        int total=nums.length*(nums.length+1)/2; //total=6
+        for(int i: nums){
+            total-=i;
+        }
+        return total;//2
+    }
+}
